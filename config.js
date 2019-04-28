835a3c192fd0773608e4e25cae793beeb35abe16
JD <jt7903@gmail.com>
2019-04-28 18:45:00 -0400

    initial

---
+++ config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  username: 'JD',
+  password: 'pass',
+  api_key: '3K2ZmyEMrXGGyR7EGBGnbti1HZNk2TZL'
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+
+var config = require('./config');
+var axios = require('axios');
+
+
+var url = `https://api.giphy.com/v1/gifs/random?api_key=${config.api_key}`;
+
+
+// Get data from an API on the web
+axios
+  .get(url)
+  .then(function (res) {
+    console.log(res.data);
+  });
+// Make a connection to a database
+
+
+
+
+
+
+
+
+
+
+
+// axios
+//   .get(url)
+//   .then(function(res) { return res.data })
+//   .then(function (data) {
+//     console.log(res.data);
+//   });
