a66d0240032c1f6378da2ab6c862cbea0bf8f3c0
JD <jt7903@gmail.com>
2019-04-28 19:14:55 -0400

    added .env

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-
-var config = require('./config');
-var axios = require('axios');
-
-
-var url = `https://api.giphy.com/v1/gifs/random?api_key=${config.api_key}`;
+require('dotenv').config();
+// var config = require('./config');
+// var axios = require('axios');
 
 
-// Get data from an API on the web
-axios
-  .get(url)
-  .then(function (res) {
-    console.log(res.data);
-  });
+
+// var url = `https://api.giphy.com/v1/gifs/random?api_key=${config.api_key}`;
+
+
+// // Get data from an API on the web
+// axios
+//   .get(url)
+//   .then(function (res) {
+//     console.log(res.data);
+//   });
 // Make a connection to a database
 
+// var env = require('./.env');
 
+console.log(process.env.API_KEY);
 
 
 
